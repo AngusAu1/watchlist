479eafd940891ebd1bebf80a45606209d226e661
AngusAu1 <pennyau1@gmail.com>
2024-11-02 11:43:40 +0000

    Add index page

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask
 from markupsafe import escape
 from flask import url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name = 'Angus Au'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 # root
 @app.route('/')
-def hello():
-    return '<h1>Hello!! Welcome to my Watchlist!</h1><img s<img src="https://media.tenor.com/s3I_IAym7_EAAAAj/rick-and-morty.gif">'
+#def hello():
+#    return '<h1>Hello!! Welcome to my Watchlist!</h1><img s<img src="https://media.tenor.com/s3I_IAym7_EAAAAj/rick-and-morty.gif">'
 
-# /index page
-@app.route('/index')
 def index():
-    return 'This is index page'
-
-# /home page
-@app.route('/home')
-def home():
-    return 'This is home page'
+    return render_template('index.html', name=name, movies=movies)
 
 # /user/<name> page
 # using escape from markupsafe
