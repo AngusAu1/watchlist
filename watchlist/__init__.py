a29230030b7bef3c18d1a91db136cd80f4cdfd14
AngusAu1 <pennyau1@gmail.com>
2024-11-05 17:52:34 +0000

    Update the comments on __init__.py

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -18,8 +18,8 @@ app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'dev'
 
 '''
-# 注意更新这里的路径，把 app.root_path 添加到 os.path.dirname() 中
-# 以便把文件定位到项目根目录
+# Please note that to update this path, add the app.root_path tot the os.path.dirname()
+# In order to help to locate the root
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 '''
